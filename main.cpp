6ce25aaed8a230b669100465c1b0432be7978784
ninidelafe <delaam18@wfu.edu>
2019-09-05 16:45:39 -0400

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,46 @@
-#include <iostream>
+/*Alexa (Nini) de la Fe
+Section B
+Project 1
+ This program will______ */
+
+
+ #include <iostream>
+#include <vector>
+#include <ctime>
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    int row=3;
+    int col=3;
+    vector<int> vec(col,0); //declares vectors
+    vector<vector<int>> matrix(row,vec);
+
+
+
+//for printing the vectors:
+
+for (int i=0;i<matrix.size();i++){
+    for (int j=0; j<matrix.at(i).size();j++){
+        cout<<
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     return 0;
 }
