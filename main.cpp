808270c69d2c9a81f8d753cd42d162097361f9f7
ninidelafe <delaam18@wfu.edu>
2019-09-06 18:05:44 -0400

    Commit 2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 /*Alexa (Nini) de la Fe
 Section B
 Project 1
- This program will______ */
-
+This program will create a matrix and ask the user to try to guess a spot to sink the battleship or spot that is
+ selected by a random number generator. */
 
  #include <iostream>
 #include <vector>
@@ -13,21 +13,28 @@ int main() {
 
     int row=3;
     int col=3;
-    vector<int> vec(col,0); //declares vectors
+    vector<int> vec (col,0); //declares vectors
     vector<vector<int>> matrix(row,vec);
 
+    cout<<"Think of a location on the matrix to attempt to sink my battleship. The matrix is the one bellow."<<endl;
 
-
-//for printing the vectors:
-
+//prints the original matrix for the user to visualize:
 for (int i=0;i<matrix.size();i++){
     for (int j=0; j<matrix.at(i).size();j++){
-        cout<<
+        cout<<matrix.at(i).at(j)<<" ";
     }
-
+    cout<<endl;
 }
 
+int userInputCol; // declares variables for the users inputs
+int userInputRow;
 
+cout<<"What column did you think of?"<<endl; //asks the user for their inputs
+cin>>userInputCol;
+cout<<"What row did you think of?"<<endl;
+cin>>userInputRow;
+
+cout<<userInputCol<<endl<<userInputRow; //prints the users inputs for testing; WILL NOT BE USED IN FINAL
 
 
 
