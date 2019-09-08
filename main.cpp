0e9a1977802f68d5575ef09ff9dac5912681fb5b
ninidelafe <delaam18@wfu.edu>
2019-09-07 22:04:45 -0400

    Commit 4

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 /*Alexa (Nini) de la Fe
 Section B
 Project 1
-This program will create a matrix and ask the user to try to guess a spot to sink the battleship or spot that is
- selected by a random number generator. */
+This program will create a matrix and ask the user to try to guess a location to attempt to sink the "battleship" or spot
+ that is selected by a random number generator. */
 
- #include <iostream>
+#include <iostream>
 #include <vector>
 #include <time.h>
 #include <string>
@@ -12,12 +12,15 @@ using namespace std;
 
 int main() {
 
-    int row=3;
-    int col=3;
+    int row=3;//declares the number of rows
+    int col=3;//declares the number of columns
     vector<string> vec (col," "); //declares vectors
     vector<vector<string>> matrix(row,vec);
-    int colRandom=0;
-    int rowRandom=0;
+    int colRandom=0;//declares the variable to save the random generated number for column
+    int rowRandom=0;//declares the variable to save the random generated number for row
+    int guess=0;//declares the variable for 
+    int userInputCol=0; // declares variables for the users inputs
+    int userInputRow=0;
 
     cout<<"Think of a location on the matrix below to attempt to sink my battleship."<<endl;
 
@@ -36,39 +39,53 @@ srand(time(0));
 colRandom= rand()% 3+1;
 rowRandom= rand()% 3+1;
 
-cout<<colRandom<<endl<<rowRandom<<endl;
 
-int userInputCol; // declares variables for the users inputs
-int userInputRow;
-
-cout<<"What column did you think of?"<<endl; //asks the user for their inputs
+cout<<"What column do you want to choose?"<<endl; //asks the user for their inputs
 cin>>userInputCol;
-cout<<"What row did you think of?"<<endl;
+cout<<"What row do you want to choose?"<<endl;
 cin>>userInputRow;
 
-cout<<userInputCol<<endl<<userInputRow<<endl; //prints the users inputs for testing; WILL NOT BE USED IN FINAL
 
 matrix.at(userInputRow-1).at(userInputCol-1)="X";
 
-    for (int i=0;i<matrix.size();i++){
-        cout<< "__________________"<<endl;
-        for (int j=0; j<matrix.at(i).size();j++){
-            cout<<"|  "<<matrix.at(i).at(j)<<" |";
+while (matrix.at(userInputRow-1).at(userInputCol-1)!=matrix.at(rowRandom-1).at(colRandom-1)) {
+
+    if (matrix.at(userInputRow - 1).at(userInputCol - 1) != matrix.at(rowRandom - 1).at(colRandom - 1)) {
+
+        for (int i = 0; i < matrix.size(); i++) {
+            cout << "__________________" << endl;
+            for (int j = 0; j < matrix.at(i).size(); j++) {
+                cout << "|  " << matrix.at(i).at(j) << " |";
+            }
+            cout << endl;
         }
-        cout<<endl;
+        cout << "__________________" << endl;
+        cout << "You missed!:( Try again!"<<endl;
+        guess++;
+
+        cout << "What column do you want to choose?"<<endl;
+        cin >> userInputCol;
+        cout << "What row do you want to choose?"<<endl;
+        cin >> userInputRow;
+
+        matrix.at(userInputRow-1).at(userInputCol-1)="X";
     }
-    cout<<"__________________"<<endl;
+    if (matrix.at(userInputRow - 1).at(userInputCol - 1) == matrix.at(rowRandom - 1).at(colRandom - 1)){
+        matrix.at(userInputRow-1).at(userInputCol-1)="*";
+        guess++;
+        cout << "You sunk my battleship! You got it on try #" << guess << "!!"<<endl;
+        for (int i = 0; i < matrix.size(); i++) {
+            cout << "__________________" << endl;
+            for (int j = 0; j < matrix.at(i).size(); j++) {
+                cout << "|  " << matrix.at(i).at(j) << " |";
+            }
+            cout << endl;
+        }
+        cout << "__________________" << endl;
+    }
 
-/* while(matrix.at(userInputCol).at(userInputRow) != matrix.at(colRandom).at(rowRandom)){
-
-
-//}
-if(matrix.at(userInputCol-1).at(userInputRow-1) == matrix.at(colRandom-1).at(rowRandom-1)){
-    cout<<"You sunk my battleship"<<endl;
 }
-else{
-    cout<<"Try again!"<<endl;
-} */
+
 
 
 
